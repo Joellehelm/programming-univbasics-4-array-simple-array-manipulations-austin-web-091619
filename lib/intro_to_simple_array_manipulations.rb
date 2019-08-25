8d433b03e517c7279bd558ccd990044c2d10f1e7
Joelle Helm <joellenhelm@gmail.com>
2019-08-25 21:12:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -25,3 +25,15 @@ end
 def using_concat(array1, array2)
   array1.concat(array2)
 end
+
+def using_insert(array, ele)
+  array.insert(ele)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
